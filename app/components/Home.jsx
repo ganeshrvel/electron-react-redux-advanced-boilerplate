394fb99fe0ef7c82bc7ed29efd40ba1e616bbcbf
Ganesh Rathinavel <ganeshrnet@live.com>
2018-09-16 19:19:11 +0530

    initial commit

---
+++ app/components/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../constants/routes.json';
+import styles from './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className={styles.container} data-tid="container">
+        <h2>Home</h2>
+        <Link to={routes.COUNTER}>to Counter</Link>
+      </div>
+    );
+  }
+}
