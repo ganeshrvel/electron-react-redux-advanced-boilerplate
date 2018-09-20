1e19ecc2d127110d7459fa272a99f4e7c1388608
Ganesh Rathinavel <ganeshrnet@live.com>
2018-09-20 18:52:55 +0530

    image optimized

--- app/containers/HomePage/index.jsx
+++ app/containers/HomePage/index.jsx
@@ -11,11 +11,16 @@ export default class Home extends Component {
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
         <Link to={routes.Counter.path}>to Counter</Link>
-        <img
+        {/*
+       
+       //Importing an image
+       <img
           src={require('../../public/images/keyboard.jpg')}
           width="100px"
           height="auto"
         />
+       
+        */}
       </div>
     );
   }
